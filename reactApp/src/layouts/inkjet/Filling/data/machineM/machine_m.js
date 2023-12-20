2e59e9f2c252934185398f408624c8e8f230d0e7
GIM-it <it@general-i.com.my>
2023-12-20 14:45:42 +0800

    Changes to be committed: 	modified:   reactApp/src/layouts/inkjet/Filling/data/machineM/machine_m.js

--- reactApp/src/layouts/inkjet/Filling/data/machineM/machine_m.js
+++ reactApp/src/layouts/inkjet/Filling/data/machineM/machine_m.js
@@ -11,16 +11,55 @@ export const Machine_MData = () => {
       { Header: "time", accessor: "time", width: "1%", Cell: ({ row }) => row.original.created_at.substr(11, 11).substr(0, 8) },
       { Header: "sequence", accessor: "counter", width: "1%", align: "left" },
       { Header: "time to reach 20pa(s)", accessor: "reach_20pa", width: "1%", align: "left" },
-      { Header: "ch1 filing DO", accessor: "ch1_do", width: "1%", align: "left" },
-      { Header: "ch2 filing DO", accessor: "ch2_do", width: "1%", align: "left" },
-      { Header: "ch3 filing DO", accessor: "ch3_do", width: "1%", align: "left" },
-      { Header: "quantity", 
-      accessor: ({counter}) =>{
-        const formattedCounter = (counter-counter + 3);
-        return formattedCounter
+      {
+        Header: "Ink temp℃",
+        accessor: "ch1_tica1",
+        width: "1%",
+        align: "left",
+        Cell: ({ value }) => {
+          const formattedValue = (Number(value) / 10).toFixed(2);
+          return formattedValue;
+        },
       },
-      width: "1%", 
-      align: "left" },
+      {
+        Header: "ch1 filing DO",
+        accessor: "ch1_do",
+        width: "1%",
+        align: "left",
+        Cell: ({ value }) => {
+          const formattedValue = (Number(value) / 100).toFixed(2);
+          return formattedValue;
+        },
+      },
+      {
+        Header: "ch2 filing DO",
+        accessor: "ch2_do",
+        width: "1%",
+        align: "left",
+        Cell: ({ value }) => {
+          const formattedValue = (Number(value) / 100).toFixed(2);
+          return formattedValue;
+        },
+      },
+      {
+        Header: "ch3 filing DO",
+        accessor: "ch3_do",
+        width: "1%",
+        align: "left",         
+        Cell: ({ value }) => {
+          const formattedValue = (Number(value) / 100).toFixed(2);
+          return formattedValue;
+        },
+      },
+      {
+        Header: "quantity",
+        accessor: ({ counter }) => {
+          const formattedCounter = (counter - counter + 3);
+          return formattedCounter
+        },
+        width: "1%",
+        align: "left"
+      },
     ],
 
     rows: machineM.data

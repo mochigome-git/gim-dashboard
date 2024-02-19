b3e1fc3b3e670f1ff443b86e8cbd8f1ee9bc6cb7
GIM-it <it@general-i.com.my>
2024-02-19 15:51:43 +0800

    Changes to be committed: 	modified:   reactApp/src/layouts/dashboard/components/Temperature/index.js

--- reactApp/src/layouts/dashboard/components/Temperature/index.js
+++ reactApp/src/layouts/dashboard/components/Temperature/index.js
@@ -14,91 +14,79 @@ function TemperatureStaticCard() {
     const open = Boolean(anchorEl);
     const [tab, setTab] = useState('NK2');
     const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
+        setAnchorEl(event.currentTarget);
     };
     const handleClose = () => {
-      setAnchorEl(null);
+        setAnchorEl(null);
     };
 
-    useEffect(() => {  
-      setArea(tab)
+    useEffect(() => {
+        setArea(tab)
     }, [tab]);
 
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('http://api.weatherapi.com/v1/current.json?key=2f8fe0a1e7c8471cb1c43209241902&q=Rawang&aqi=yes');
-  
-          if (!response.ok) {
-            throw new Error('Failed to fetch data');
-          }
-  
-          const result = await response.json();
-          setData(result);
-          setDescription(result?.current.condition.text);
-        } catch (error) {
-          console.error('Error fetching data:', error.message);
-        }
-      };
-  
-      fetchData();
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://api.weatherapi.com/v1/current.json?key=2f8fe0a1e7c8471cb1c43209241902&q=Rawang&aqi=yes');
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+
+                const result = await response.json();
+                setData(result);
+                setDescription(result?.current.condition.text);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+
+        fetchData();
     }, []);
 
     useEffect(() => {
         const main = data?.current.condition.text;
 
         switch (main) {
-          case "Snow":
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/snow.gif')"
-            );
-            break;
-          case "Clouds" || "Partly Cloudy":
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')"
-            );
-            break;
-          case "Fog":
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/fog.gif')"
-            );
-            break;
-          case "Rain":
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/rain.gif')"
-            );
-            break;
-          case "Clear":
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')"
-            );
-            break;
-          case "Thunderstorm":
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/thunderstorm.gif')"
-            );
-            break;
-          default:
-            setBGGif(
-              "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')"
-            );
-            break;
-        };
-  }, []);
+            case "Snow":
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/snow.gif')");
+                break;
+            case "Clouds":
+            case "Partly Cloudy":
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')");
+                break;
+            case "Fog":
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/fog.gif')");
+                break;
+            case "Rain":
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/rain.gif')");
+                break;
+            case "Clear":
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')");
+                break;
+            case "Thunderstorm":
+            case "Patchy light rain with thunder":
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/thunderstorm.gif')");
+                break;
+            default:
+                setBGGif("url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')");
+                break;
+        }
+    }, [data]);
 
-  return (
-    <TemperatureCard
-    anchorEl={anchorEl}
-    open={open}
-    tab={tab}
-    setTab={setTab}
-    handleClick={handleClick}
-    handleClose={handleClose}
-    data={tempData?.latest}
-    description={description}
-    bgGif={bgGif}
-    />
-  );
+    return (
+        <TemperatureCard
+            anchorEl={anchorEl}
+            open={open}
+            tab={tab}
+            setTab={setTab}
+            handleClick={handleClick}
+            handleClose={handleClose}
+            data={tempData?.latest}
+            description={description}
+            bgGif={bgGif}
+        />
+    );
 }
 
 

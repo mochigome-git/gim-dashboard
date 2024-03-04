a78a80eb6498ba0c808e132bfd526faca5de1028
GIM-it <it@general-i.com.my>
2024-03-04 10:40:35 +0800

    Changes to be committed: 	new file:   reactApp/src/layouts/inkjet/Filling/data/machineT/machine_t_degas.js

---
+++ reactApp/src/layouts/inkjet/Filling/data/machineT/machine_t_degas.js
@@ -0,0 +1,21 @@
+// @mui material components
+
+import { useContext } from "react";
+import { MachineTContext } from "../../../../../lib/realtime/inkjet/machineT_realtime";
+
+export const Machine_t_degas_Data = () => {
+  const { machineT } = useContext(MachineTContext)
+
+  return {
+    columns: [
+      { Header: "date", accessor: "date", width: "1%", Cell: ({ row }) => row.original.created_at.substr(0, 10) },
+      { Header: "time", accessor: "time", width: "1%", Cell: ({ row }) => row.original.created_at.substr(11, 11).substr(0, 8) },
+      { Header: "Maximum Flow", accessor: "pica1_max", width: "1%", align: "left" },
+      { Header: "Average Flow", accessor: "pica1_average", width: "1%", align: "left" },
+    ],
+
+    rows: machineT.degas
+  };
+}
+
+export default Machine_t_degas_Data;
